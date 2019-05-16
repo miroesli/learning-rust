67dc63f148c25fb16cae11ae28596bf42d7e569d
BuildTools <misha.roesli@gmail.com>
2019-05-15 20:36:32 -0700

    created base guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello I'm Bob! Guess the number I'm thinking of! It is anything from 1 to 100.");
+    print!("My friend Fred will also try to guess. Try to guess it before him!");
+
+    let mut myguess = String::new();
+
+    io::stdin().read_line(&mut myguess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", myguess);
 }
